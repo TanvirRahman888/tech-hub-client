fce2251c3d40ac6665c69c6d5f810dcc60816a77
TanvirRahman888 <tanvirrahman8888@gmail.com>
2025-06-16 04:06:34 +0600

    Navbar Update

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -43,7 +43,7 @@ const NavBar = () => {
           {user ? <div className='flex gap-3 items-center'><h3> {user?.email} </h3> <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handelLogout}> Log Out</button>
           </div> :
             <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              <Link href={"/register"}>Get started</Link>
+              <Link href={"/login"}>Log In</Link>
             </button>
           }
         </div>
